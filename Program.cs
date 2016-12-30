191f7bc7fcfd1761d5d3f654a10947388a5616f6
Veljko Knezevic <veljko.knezevic@gmail.com>
2016-12-30 17:52:22 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorDemo
+{
+    static class Program
+    {
+        static IEnumerable<int> Populate(int max)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 1; i <= max; i++)
+                list.Add(i);
+
+            return list;
+        }
+        static void Main()
+        {
+            int n = 100000000;
+            long sum = 0;
+
+            Stopwatch sw = Stopwatch.StartNew();
+            int stopAt = 10000000;
+
+            foreach (int value in Populate(n))
+            {
+                sum += value;
+                if (value % stopAt == 0)
+                {
+                    Console.WriteLine("{0}M\tMem={1}Mbyte\tTime={2}msec",
+                                        value / 1000000,
+                                        GC.GetTotalMemory(false) / 1024 / 1024,
+                                        sw.ElapsedMilliseconds);
+                }
+            }
+
+            long expectedSum = (long)n * ((long)n + 1) / 2;
+            Console.WriteLine("Sum {{1.. {0:#,##0} = {1:#,##0}}} = {1:#,##0}; expected {2:#,##0}", n, sum, expectedSum);
+
+            Console.ReadLine();
+        }
+    }
+}
