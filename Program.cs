39550ff0c9ac3643a909fb29d6f79aa9a7a00b96
vknez95 <veljko.knezevic@gmail.com>
2016-12-31 16:33:04 -0500

    applying infinite sequence part2

--- Program.cs
+++ Program.cs
@@ -17,25 +17,8 @@ namespace IteratorDemo
         }
         static void Main()
         {
-            int n = 1000000000;
-            long sum = 0;
-
-            Stopwatch sw = Stopwatch.StartNew();
-            int stopAt = 10000000;
-
-            foreach (int value in Populate())
-            {
-                sum += value;
-                if (value % stopAt == 0)
-                {
-                    Console.WriteLine("{0}M\tMem={1}Mbyte\tTime={2}msec",
-                                        value / 1000000,
-                                        GC.GetTotalMemory(false) / 1024 / 1024,
-                                        sw.ElapsedMilliseconds);
-                }
-                if (value >= n)
-                    break;
-            }
+            int n = 100000000;
+            long sum = Populate().TakeWhile(x => x <= n).Sum(x => (long)x);
 
             long expectedSum = (long)n * ((long)n + 1) / 2;
             Console.WriteLine("Sum {{1.. {0:#,##0} = {1:#,##0}}} = {1:#,##0}; expected {2:#,##0}", n, sum, expectedSum);
